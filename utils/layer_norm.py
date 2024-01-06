93bdb940b06ef60b0255ff3ce272e41aadbf2a0d
NOAH <letgoofthepizza@gmail.com>
2024-01-07 01:49:16 +0900

    layer norm

---
+++ utils/layer_norm.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class LayerNormalization:
+    
+    def __init__(self, eps: float = 10**-6): # We define epsilon as 0.000001 to avoid division by zero
+        self.eps = eps
+        
+        # 학습은 구현 안함
+        self.alpha = np.ones((1,)) 
+        self.bias = np.zeros((1,))
+        
+    def forward(self, x):
+        mean = x.mean(axis = -1, keepdims = True) # Computing the mean of the input data. Keeping the number of dimensions unchanged
+        std = x.std(axis = -1, keepdims = True) # Computing the standard deviation of the input data. Keeping the number of dimensions unchanged
+        
+        # Returning the normalized input
+        return self.alpha * (x-mean) / (std + self.eps) + self.bias
---
+++ utils/pt_layer_norm.py
@@ -0,0 +1,21 @@
+import pytorch
+import pytorch.nn as nn
+
+class LayerNormalization(nn.Module):
+    
+    def __init__(self, eps: float = 10**-6) -> None: # We define epsilon as 0.000001 to avoid division by zero
+        super().__init__()
+        self.eps = eps
+        
+        # We define alpha as a trainable parameter and initialize it with ones
+        self.alpha = nn.Parameter(torch.ones(1)) # One-dimensional tensor that will be used to scale the input data
+        
+        # We define bias as a trainable parameter and initialize it with zeros
+        self.bias = nn.Parameter(torch.zeros(1)) # One-dimensional tenso that will be added to the input data
+        
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim = True) # Computing the mean of the input data. Keeping the number of dimensions unchanged
+        std = x.std(dim = -1, keepdim = True) # Computing the standard deviation of the input data. Keeping the number of dimensions unchanged
+        
+        # Returning the normalized input
+        return self.alpha * (x-mean) / (std + self.eps) + self.bias
